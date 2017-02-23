f6d046a30c2f680db73caa64ded6f8657c520038
Jeremy Kalsow <jeremy.kalsow@outlook.com>
2017-02-23 17:53:37 -0600

    working socket IO example

--- index.js
+++ index.js
@@ -37,6 +37,22 @@ whiteCards.readInCards().then(resolveMessage => {
 app.use(express.static(__dirname + '/node_modules'));
 app.get('/', function(req, res, next)
 {
-   res.sendFile(__dirname + 'index.html');
+   res.sendFile(__dirname + '/index.html');
 });
+
+
+io.on('connection', function(client) {
+    console.log('Client connected...');
+
+    client.on('join', function(data) {
+        console.log(data);
+    });
+
+    client.on('messages', function(data) {
+           client.emit('broad', data);
+           client.broadcast.emit('broad',data);
+    });
+
+});
+
 server.listen(4200);
