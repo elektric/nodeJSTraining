41c9d0339a0dad5072c6033cc0cc8ccb729fa7d7
Jeremy Kalsow <jeremy.kalsow@outlook.com>
2017-02-23 17:14:25 -0600

    added jquery

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-var http = require('http');
 var BlackCards = require('./BlackCards');
 var WhiteCards = require('./WhiteCards');
-
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
 let blackCards = new BlackCards('blackCards.txt');
 let whiteCards = new WhiteCards('whiteCards.txt');
 
 blackCards.readInCards().then(resolveMessage => {
 
-      for (let blackCard of blackCards.allBlackCards)
-      {
-         console.log(blackCard);
-      }
+      // for (let blackCard of blackCards.allBlackCards)
+      // {
+      //    console.log(blackCard);
+      // }
       console.log(resolveMessage);
    },
    (err) => {
@@ -21,13 +23,20 @@ blackCards.readInCards().then(resolveMessage => {
 
 whiteCards.readInCards().then(resolveMessage => {
 
-      for (let whiteCard of whiteCards.allWhiteCards)
-      {
-         console.log(whiteCard);
-      }
+      // for (let whiteCard of whiteCards.allWhiteCards)
+      // {
+      //    console.log(whiteCard);
+      // }
       console.log(resolveMessage);
    },
    (err) => {
       console.log(err);
    }
 );
+
+app.use(express.static(__dirname + '/node_modules'));
+app.get('/', function(req, res, next)
+{
+   res.sendFile(__dirname + 'index.html');
+});
+server.listen(4200);
